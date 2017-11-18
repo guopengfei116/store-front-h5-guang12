e0d1c1e76e985586dc7ed3d2fa039a1d08f34305
PengFei <626138959@qq.com>
2017-11-18 09:24:28 +0800

    脚手架测试

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,24 +1,15 @@
 <template>
-  <main>
-      <p>{{ msg }}</p>
-  </main>
+    <div>
+        测试
+    </div>
 </template>
 
 <script>
-// 这里vue要求我们对外暴漏一个组件的配置项
-// 这里因为template已经在上面独立编写了,vue-loader到时候会帮我们处理,所以配置项中就不用写template了
-export default {
-    data() {
-        return {
-            msg: '测试vue文件是否可用'
-        }
+    export default {
+        
     }
-}
 </script>
 
-<style>
-p {
-    font-size: 30px;
-    color: red;
-}
+<style scoped>
+
 </style>
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,15 @@
-// 要把组件渲染到页面中, 我们就必须要导入Vue这个库, 然后才可以new他
-// es6语法导入的Vue与我们在浏览器中使用的Vue有点小区别, 失去了浏览器模版编译功能,
-// 失去该功能的原因是, 你使用es6语法, vue知道你一定使用了构建工具, 那么使用构建工具,
-// 你应该会使用vue-loader, 使用了vue-loader他会在本地打包时把模版编译好, 
-// 在浏览器中直接使用, 相比咱们以往的方式性能会有提升.
-import Vue from 'Vue';
-import App from '../component/App.vue';
+// from后面的路径, 如果含有./ ../那么就相对于当前文件找文件
+// 如果没有, 那么就会去node_modules里面找对应的包
+// 1.1 导入第三方包
+import Vue from 'vue';
 
+// 2.1 导入根组件
+import AppComponent from '../component/App.vue';
+
+// 2.2 渲染根组件, 启动项目
 new Vue({
     el: '#app',
-    render: function(c) {
-        return c(App);  // 这个函数需要我们返回一个组件, 把返回的组件渲染到el标签中
+    render(createNode) {
+        return createNode(AppComponent);
     }
 });
