7459b240bbf0dc831c1de4a7980f7566577ecc4f
PengFei <626138959@qq.com>
2017-11-18 16:40:36 +0800

    实现首页6宫格

--- src/component/home/home.vue
+++ src/component/home/home.vue
@@ -3,12 +3,50 @@
         <!-- 轮播图 -->
         <mt-swipe :auto="10000">
             <mt-swipe-item v-for="item in lunbos" v-bind:key="item.url">
-                <a v-bind:href="item.url">
+                <router-link v-bind:href="item.url">
                     <img v-bind:src="item.img">
-                </a>
+                </router-link>
             </mt-swipe-item>
         </mt-swipe>
-        <!-- 九宫格 -->
+        <!-- 六宫格 -->
+        <ul class="mui-table-view mui-grid-view mui-grid-9">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <router-link to="/">
+                    <span class="mui-icon mui-icon-home"></span>
+                    <div class="mui-media-body">首页</div>
+                </router-link>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <router-link to="/news/list">
+                    <span class="mui-icon mui-icon-chat"></span>
+                    <div class="mui-media-body">热点新闻</div>
+                </router-link>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <router-link to="/photo/list">
+                    <span class="mui-icon mui-icon-image"></span>
+                    <div class="mui-media-body">图片分享</div>
+                </router-link>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <router-link to="/">
+                    <span class="mui-icon mui-icon-search"></span>
+                    <div class="mui-media-body">搜索</div>
+                </router-link>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <router-link to="/">
+                    <span class="mui-icon mui-icon-phone"></span>
+                    <div class="mui-media-body">联系我们</div>
+                </router-link>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <router-link to="/">
+                    <span class="mui-icon mui-icon-info"></span>
+                    <div class="mui-media-body">关于我们</div>
+                </router-link>
+            </li>
+        </ul>
     </article>
 </template>
 
