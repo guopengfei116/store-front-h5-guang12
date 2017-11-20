67318363bdff837c7678edd23840d5a8f33d385b
PengFei <626138959@qq.com>
2017-11-20 11:32:29 +0800

    完成新闻详情

--- src/component/news/news_detail.vue
+++ src/component/news/news_detail.vue
@@ -1,15 +1,44 @@
 <template>
-    <div>
-        新闻详情
-    </div>
+    <article>
+        <!-- 使用mui的卡片视图布局, 注意: 卡片视图都是flex, 这里把他改成block -->
+        <div class="mui-card">
+            <div class="mui-card-header">
+                <h4>{{ newsDetail.title }}</h4>
+                <p class="mui-ellipsis">
+                    <span>创建时间: {{ newsDetail.add_time | date }}</span>
+                    <span>点击数: {{ newsDetail.click }}</span>
+                </p>
+            </div>
+            <!-- content内容中含有html标签, 要让它正常展示, 必须使用v-html -->
+            <div class="mui-card-footer" v-html="newsDetail.content"></div>
+        </div>
+    </article>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    data() {
+        return {
+            newsDetail: {}
+        };
+    },
+
+    methods: {
+        // 获取新闻详情, 注意这里接口返回的是一个数组, 我们需要通过下标那新闻详情对象
+        getNewsDetail() {
+            this.axios.get(this.api.getND + this.$route.params.id)
+            .then( rsp => this.newsDetail = rsp.data.message[0] )
+        }
+    },
+
+    created() {
+        this.getNewsDetail();
     }
+}
 </script>
 
 <style scoped>
-
+.mui-card-header, .mui-card-footer {
+    display: block;
+}
 </style>
