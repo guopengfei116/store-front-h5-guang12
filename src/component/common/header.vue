c6c3264dd8e16478ae6479a54d4e8cd498f3b62f
PengFei <626138959@qq.com>
2017-11-20 11:42:41 +0800

    头部添加返回按钮

--- src/component/common/header.vue
+++ src/component/common/header.vue
@@ -1,6 +1,12 @@
 <template>
     <header>
-        <mt-header fixed title="小买卖"></mt-header>
+        <mt-header fixed title="小买卖">
+            <!-- 返回按钮只在非首页出现 -->
+            <!-- 通过$route.path那到当前路径比较 -->
+            <!-- 通过$router.go(-1)方法实现页面返回功能 -->
+            <mt-button v-if="$route.path != '/index'" v-on:click="$router.go(-1)"
+                icon="back" slot="left"></mt-button>
+        </mt-header>
         <i></i>
     </header>
 </template>
