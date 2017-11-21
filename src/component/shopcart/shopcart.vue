806d7af96db4b2db40b526191479668f3fd7bb92
PengFei <626138959@qq.com>
2017-11-21 17:41:34 +0800

    总价计算与默认开启购买选项

--- src/component/shopcart/shopcart.vue
+++ src/component/shopcart/shopcart.vue
@@ -26,7 +26,7 @@
       <div class="total_val">
         <ul>
           <li>总计（不含运费）</li>
-          <li>已勾选商品{{ selectedGoodsTotal }}件,总价:￥1000元</li>
+          <li>已勾选商品{{ selectedGoodsTotal }}件,总价:￥{{ selectedGoodsPriceTotal }}元</li>
         </ul>
       </div>
       <div class="total_btn">
@@ -52,7 +52,13 @@ export default {
     getBuyGoodsList() {
       let ids = Object.keys(storage.get('goodsBuyData')).join(',');
       this.axios.get(this.api.shopcL + ids)
-      .then( rsp => this.buyGoodsList = rsp.data.message );
+      .then( 
+        rsp => {
+          // 给每个商品补充一个isSelected属性, 默认值为true
+          rsp.data.message.forEach(goods => goods.isSelected = true);
+          this.buyGoodsList = rsp.data.message;
+        }
+      );
     },
 
     // 通过商品id获取对应数据
@@ -67,20 +73,30 @@ export default {
 
   computed: {
     // 已勾选的商品总件数
+    // selectedGoodsTotal() {
+    //   // 遍历购物车商品列表, 如果该商品是选中状态, 那么获取这个商品的购买数量累加起来
+    //   let sum = 0;
+    //   this.buyGoodsList.forEach((goods) => {
+    //     if(goods.isSelected) {
+    //       sum += this.getBuyCountById(goods.id);
+    //     }
+    //   });
+    //   return sum;
+    // },
+
     selectedGoodsTotal() {
-      // 遍历购物车商品列表, 如果该商品是选中状态, 那么获取这个商品的购买数量累加起来
-      let sum = 0;
-      this.buyGoodsList.forEach((goods) => {
-        if(goods.isSelected) {
-          sum += this.getBuyCountById(goods.id);
-        }
-      });
-      return sum;
+      return this.buyGoodsList.reduce((sum, goods) => {
+        return goods.isSelected? sum + this.getBuyCountById(goods.id) : sum;
+      }, 0);
     },
+
 
     // 已勾选的商品总价
     selectedGoodsPriceTotal() {
-
+      // 遍历购物车商品列表, 如果该商品是选中状态, 那么获取这个商品的价格 * 购买数量, 然后结果累加起来
+      return this.buyGoodsList.reduce((sum, goods) => {
+        return goods.isSelected? sum + this.getBuyCountById(goods.id) * goods.sell_price : sum;
+      }, 0);
     }
   }
 };
